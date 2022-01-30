c70fb54948cf5d72436efb007583b2128cf12a76
Oskar Figiel <qyzdo@outlook.com>
2022-01-30 20:04:49 +0100

    Created view for presenting search result

--- ZavenQr/ResultPresentation/ResultViewController.swift
+++ ZavenQr/ResultPresentation/ResultViewController.swift
@@ -19,12 +19,94 @@ final class ResultViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private let mainLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let secondLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = .systemGray
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
+        return label
+    }()
+
+    private let cancelButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setBackgroundImage(UIImage(systemName: "x.circle.fill"), for: .normal)
+        button.tintColor = .systemGray3
+        return button
+    }()
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "trash")
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.systemGray5.cgColor
+        return imageView
+    }()
+
+    private let mainButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemGray4
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
 
     private func setupView() {
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+
+        [mainLabel, secondLabel, cancelButton, imageView, mainButton].forEach{ view.addSubview($0) }
+
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            cancelButton.heightAnchor.constraint(equalToConstant: 35),
+            cancelButton.widthAnchor.constraint(equalToConstant: 35),
+
+            mainLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 10),
+            mainLabel.trailingAnchor.constraint(lessThanOrEqualTo: cancelButton.leadingAnchor, constant: -10),
+
+            secondLabel.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 5),
+            secondLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            secondLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+
+            imageView.topAnchor.constraint(greaterThanOrEqualTo: secondLabel.bottomAnchor, constant: 10),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+
+            mainButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 30),
+            mainButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mainButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            mainButton.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            mainButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+        if resultViewModel.isFromScanner {
+            mainLabel.font = UIFont.systemFont(ofSize: 25)
+            secondLabel.font = UIFont.systemFont(ofSize: 15)
+        } else {
+            mainLabel.font = UIFont.systemFont(ofSize: 20)
+            secondLabel.font = UIFont.systemFont(ofSize: 15)
+        }
+    }
     }
 }
